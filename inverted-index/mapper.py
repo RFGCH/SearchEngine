a65f3f6fe0ff8e3d9f57be7100913c2db61c1b02
Rubén Félix Guzmán Chiroque <54367628+RFGCH@users.noreply.github.com>
2023-11-06 21:51:24 -0500

    Update mapper.py

--- inverted-index/mapper.py
+++ inverted-index/mapper.py
@@ -288,6 +288,14 @@ class PorterStemmer:
                 self.k = self.k - 1
         if self.b[self.k] == 'l' and self.doublec(self.k) and self.m() > 1:
             self.k = self.k -1
+        if self.b[self.k] == '.':
+            a = self.m()
+            if a > 1 or (a == 1 and not self.cvc(self.k-1)):
+                self.k = self.k - 1
+        if self.b[self.k] == ',':
+            a = self.m()
+            if a > 1 or (a == 1 and not self.cvc(self.k-1)):
+                self.k = self.k - 1
 
     def stem(self, p, i, j):
         """In stem(p,i,j), p is a char pointer, and the string to be stemmed
